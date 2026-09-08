86eb580bea645636a0b80f1997876e9f96e2de66
manukdir <manurodriguez251822@gmail.com>
2026-09-08 01:16:53 -0300

    refactor: centralizar datos de productos en productos.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,43 @@
 // CATÁLOGO DE PRODUCTOS
+// SONIDO VIVO
 
+
+// 1. MOSTRAR DATOS EN CONSOLA
+
+
+function mostrarResumenProductos() {
+
+    console.log("Catálogo Sonido Vivo");
+
+
+    for (const producto of productos) {
+
+        console.log(
+            `${producto.codigo} - ${producto.nombre}`
+        );
+
+        console.log(
+            `Marca: ${producto.marca}`
+        );
+
+        console.log(
+            `Modelo: ${producto.modelo}`
+        );
+
+        console.log(
+            `Stock: ${producto.stock}`
+        );
+
+        console.log(
+            `Precio: $${producto.precio}`
+        );
+
+    }
+
+}
+
+
+mostrarResumenProductos();
 
 // La lógica dinámica del catálogo
 // se implementará en el siguiente avance.
---
+++ assets/js/productos.js
@@ -0,0 +1,44 @@
+const productos = [
+
+    {
+        id: 1,
+        codigo: "GE001",
+        categoria: "Guitarras Eléctricas",
+        nombre: "Guitarra Eléctrica Stratocaster",
+        marca: "Squier",
+        modelo: "Affinity Strat",
+        imagen: "assets/img/guitarra-schecter.jpg",
+        stock: 5,
+        precio: 249990,
+        descripcion:
+            "Cuerpo de álamo, mástil de arce, pastillas SSS."
+    },
+
+    {
+        id: 2,
+        codigo: "MI001",
+        categoria: "Micrófonos",
+        nombre: "Micrófono Dinámico Cardioide",
+        marca: "Shure",
+        modelo: "SM58",
+        imagen: "assets/img/microfono.jpg",
+        stock: 8,
+        precio: 149990,
+        descripcion:
+            "Estándar industria para voz en vivo."
+    },
+
+    {
+        id: 3,
+        codigo: "AM001",
+        categoria: "Amplificadores",
+        nombre: "Amplificador Guitarra 15W",
+        marca: "Fender",
+        modelo: "Frontman 15G",
+        imagen: "assets/img/amplificador-behringe.jpg",
+        stock: 5,
+        precio: 99990,
+        descripcion:
+            "15W, distorsión incorporada, entrada auxiliar."
+    },
+];
